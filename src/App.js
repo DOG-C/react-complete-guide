97d3bc6fb2398ad7138aa9369dfdf46b0bb5f6bf
dogc <caoyuqi1996@hotmail.com>
2023-04-26 18:00:16 +0200

    Implemented passing value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 4),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 4),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 4),
+    },
+    {
+      id: "e4",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 4),
+    },
+    {
+      id: "e5",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 4),
+    },
+  ];
+
   return (
     <div>
       <h1>Get start!</h1>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} ></ExpenseItem>
     </div>
   );
 }
