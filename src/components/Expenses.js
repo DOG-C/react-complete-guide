e9069efd07f51832e31a254e535d56a7373b1fc0
dogc <caoyuqi1996@hotmail.com>
2023-04-26 18:29:20 +0200

    Added map function in Expenses.js for read all of the data

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,11 +4,13 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+      {props.items.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
     </div>
   );
 }
