77e71e17b73cae584eef5bed246b4f362b51d111
dogc <caoyuqi1996@hotmail.com>
2023-04-26 17:42:23 +0200

    Replaced static code with variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  return (
+    const expenseDate = new Date(2023, 1, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+    return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$295.67</div>
-      </div>
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+        </div>
     </div>
-  );
+    );
 }
 
 export default ExpenseItem;
