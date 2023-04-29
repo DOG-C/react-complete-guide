f8e4350a982281e69deccb5b1bfeb7711fd077fd
dogc <caoyuqi1996@hotmail.com>
2023-04-29 23:45:36 +0200

    Implemented displaying  filtered content in 3 ways

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,10 +12,23 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(item => {
+  const filteredExpenses = props.items.filter((item) => {
     return item.date.getFullYear().toString() === filteredYear;
-
   });
+
+  // 3. 命名变量以后采用if语句
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((item) => (
+      <ExpenseItem
+        key={item.id} //很重要，可以让React Dom是识别唯一需要更新的项，每次加上就好
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -24,14 +37,32 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {filteredExpenses.map((item) => (
-          <ExpenseItem
-            key={item.id} //很重要，可以让React Dom是识别唯一需要更新的项，每次加上就好
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        ))}
+        {/* 2. 利用JS特点的做法
+        JS会在&&前的内容判断为真之后，直接执行&&后面的内容 */}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        { filteredExpenses > 0 && filteredExpenses.map((item) => (
+            <ExpenseItem
+              key={item.id} //很重要，可以让React Dom是识别唯一需要更新的项，每次加上就好
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+              />
+        ))} */}
+        {/* 1. 传统三元式做法 */}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((item) => (
+            <ExpenseItem
+              key={item.id} //很重要，可以让React Dom是识别唯一需要更新的项，每次加上就好
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          ))
+        )} */}
+
+        {expensesContent}
       </Card>
     </div>
   );
