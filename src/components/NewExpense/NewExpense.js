6c42850d91f294bfdbf974df87fc3e9f99a054a2
dogc <caoyuqi1996@hotmail.com>
2023-04-30 01:36:43 +0200

    Implemeted unfolding the new expenses content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEdting] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random.toString(),
     };
-
     props.onAddExpense(expenseData);
+    setIsEdting(false);
+  };
+
+  const startEdtingHandler = () => {
+    setIsEdting(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEdting(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEdtingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
