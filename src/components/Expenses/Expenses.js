93c889fe7313fac738aeaf0bccd992e60b526cd3
dogc <caoyuqi1996@hotmail.com>
2023-04-30 03:34:21 +0200

    Implemented chart with dynamic display

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
-import ExpensesList from "./ExpensesList"
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -48,7 +48,7 @@ const Expenses = (props) => {
             />
           ))
         )} */}
-
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </li>
